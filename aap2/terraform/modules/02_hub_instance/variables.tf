ace6ad0db7c7b22f68e4b0381f643acda699048a
kubealex <al.rossi87@gmail.com>
2022-03-20 13:08:56 +0100

    Hub module OK

--- aap2/terraform/modules/02_hub_instance/variables.tf
+++ aap2/terraform/modules/02_hub_instance/variables.tf
@@ -1,19 +1,19 @@
 variable hostname {
   type        = string
   description = "Hostname of the VM"
-  default     = "client"
+  default     = "hub"
 }
 
 variable domain {
   type        = string
   description = "Domain for the virtual machine fqdn"
-  default     = "satellitedemo.labs"
+  default     = "aapdemo.labs"
 }
  
 variable memory {
   type        = number
   description = "Amount of memory in GBs for the VM"
-  default     = 4
+  default     = 16
 }
 
 variable cpu {
@@ -22,40 +22,28 @@ variable cpu {
   default     = 2
 }
 
-variable kickstart_image_rhel7 {
-  type        = string
-  description = "Path for the kickstart image for RHEL7"
-  default     = "rhel7-oemdrv.img"
-}
-
 variable kickstart_image_rhel8 {
   type        = string
   description = "Path for the kickstart image for RHEL8"
-  default     = "rhel8-oemdrv.img"
+  default     = "rhel8-hub-oemdrv.img"
 }
 
 variable libvirt_network {
   type        = string
   description = "Name of libvirt network to be used for the VM"
-  default     = "satellite-lab"
+  default     = "aap-lab"
 }
 
 variable libvirt_pool {
   type        = string
   description = "Name of libvirt pool to be used for the VM"
-  default     = "satellite-lab"
+  default     = "aap-lab"
 }
 
 variable disk_size {
   type        = number
   description = "Size in GBs of root volume for the VM"
   default     = 30
-}
-
-variable os_image_rhel7 {
-  type        = string
-  description = "URL/path of the image to be used for the VM provisioning"
-  default     = "rhel7.iso"
 }
 
 variable os_image_rhel8 {
